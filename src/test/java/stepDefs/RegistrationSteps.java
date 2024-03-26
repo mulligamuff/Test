76e7779682811f5e764db8937c9bd5c935649ac1
Muthoz <muthoz.gamer@gmail.com>
2024-03-26 23:44:10 +0100

    Inlämningsuppgift Automation

--- src/test/java/stepDefs/RegistrationSteps.java
+++ src/test/java/stepDefs/RegistrationSteps.java
@@ -2,10 +2,12 @@ package stepDefs;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -14,6 +16,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.Assert.*;
+
+import java.time.Duration;
 
 public class RegistrationSteps {
 
