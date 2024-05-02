c716ce5f97b9eaaff6e7cd0bf34fbcfc7906cd06
Muthoz <muthoz.gamer@gmail.com>
2024-05-02 17:18:15 +0200

    Inlämningsuppgift Automation

--- src/test/java/stepDefs/RegistrationSteps.java
+++ src/test/java/stepDefs/RegistrationSteps.java
@@ -3,6 +3,7 @@ package stepDefs;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,7 +27,6 @@ public class RegistrationSteps {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
     }
 
@@ -37,40 +37,30 @@ public class RegistrationSteps {
 
     @When("User enters valid user details")
     public void user_enters_valid_user_details() {
-        enterUserDetails("John", "Doe", "john.doe@example.com", "password123", true);
-    }
-
-    @When("User enters valid user details without last name")
-    public void user_enters_valid_user_details_without_last_name() {
-        enterUserDetails("John", "", "john.doe@example.com", "password123", true);
-    }
-
-    @When("User enters valid user details with mismatched password")
-    public void user_enters_valid_user_details_with_mismatched_password() {
-        enterUserDetails("John", "Doe", "john.doe@example.com", "password123", false);
-    }
-
-    @When("User enters valid user details without accepting terms and conditions")
-    public void user_enters_valid_user_details_without_accepting_terms_and_conditions() {
-        enterUserDetails("John", "Doe", "john.doe@example.com", "password123", false);
+        enterUserDetails("25/05/1994","John", "Doe", "muthoz.gamer+EC@gmail.com", "muthoz.gamer+EC@gmail.com", "password123", "password123", true);
     }
 
     @When("Clicks on the button to submit the details")
     public void clicks_on_the_button_to_submit_the_details() {
-        WebElement submitButton = driver.findElement(By.id("submit"));
+        WebElement submitButton = driver.findElement(By.cssSelector(".btn.btn-big.red[value='CONFIRM AND JOIN']"));
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         submitButton.click();
     }
 
     @Then("User should be registered successfully")
     public void user_should_be_registered_successfully() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Uppdatera tidsangivelsen här
-        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("successMessage")));
+        WebElement successMessage = until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[contains(text(),'THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND')]")));
         assertTrue(successMessage.isDisplayed());
     }
 
+
     @Then("Registration should fail with an error message")
     public void registration_should_fail_with_an_error_message() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Uppdatera tidsangivelsen här
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("errorMessage")));
         assertTrue(errorMessage.isDisplayed());
     }
@@ -80,22 +70,38 @@ public class RegistrationSteps {
         driver.quit();
     }
 
-    private void enterUserDetails(String firstName, String lastName, String email, String password, boolean acceptTerms) {
-        WebElement firstNameField = driver.findElement(By.id("firstName"));
+    private void clickCheckboxById(String id){
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("document.getElementById('" + id + "').click();");
+    }
+
+    private void enterUserDetails(String dateOfBirth, String firstName, String lastName, String eMail, String confEmail, String password, String confPassword, boolean acceptTerms) {
+        WebElement dateOfBirthField = driver.findElement(By.id("dp"));
+        dateOfBirthField.sendKeys(dateOfBirth);
+
+        WebElement firstNameField = driver.findElement(By.id("member_firstname"));
         firstNameField.sendKeys(firstName);
 
-        WebElement lastNameField = driver.findElement(By.id("lastName"));
+        WebElement lastNameField = driver.findElement(By.id("member_lastname"));
         lastNameField.sendKeys(lastName);
 
-        WebElement emailField = driver.findElement(By.id("email"));
-        emailField.sendKeys(email);
+        WebElement emailField = driver.findElement(By.id("member_emailaddress"));
+        emailField.sendKeys(eMail);
 
-        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement confEmailField = driver.findElement(By.id("member_confirmemailaddress"));
+        confEmailField.sendKeys(confEmail);
+
+        WebElement passwordField = driver.findElement(By.id("signupunlicenced_password"));
         passwordField.sendKeys(password);
 
-        if (!acceptTerms) {
-            WebElement termsCheckbox = driver.findElement(By.id("termsCheckbox"));
-            termsCheckbox.click();
+        WebElement confPasswordField = driver.findElement(By.id("signupunlicenced_confirmpassword"));
+        confPasswordField.sendKeys(confPassword);
+
+        if (acceptTerms) {
+            clickCheckboxById("sign_up_25");
+            clickCheckboxById("sign_up_26");
+            clickCheckboxById("fanmembersignup_agreetocodeofethicsandconduct");
         }
+
     }
 }
--- src/test/resources/features/registration.feature
+++ src/test/resources/features/registration.feature
@@ -6,20 +6,3 @@ Feature: Registration Functionality
     And Clicks on the button to submit the details
     Then User should be registered successfully
 
-  Scenario: Registration fails due to missing last name
-    Given User is on the registration page
-    When User enters valid user details without last name
-    And Clicks on the button to submit the details
-    Then Registration should fail with an error message
-
-  Scenario: Registration fails due to mismatched password
-    Given User is on the registration page
-    When User enters valid user details with mismatched password
-    And Clicks on the button to submit the details
-    Then Registration should fail with an error message
-
-  Scenario: Registration fails due to not accepting terms and conditions
-    Given User is on the registration page
-    When User enters valid user details without accepting terms and conditions
-    And Clicks on the button to submit the details
-    Then Registration should fail with an error message
